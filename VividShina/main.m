89a438e9ff9fb2f41899bf248fc0fa9c6c34cb6b
black apple <wenzhou.1991@163.com>
2013-11-27 14:40:56 +0800

    Initial Commit

---
+++ VividShina/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VividShina
+//
+//  Created by icreative-mini on 13-11-27.
+//  Copyright (c) 2013年 icreative-mini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ VividShinaTests/VividShinaTests.m
@@ -0,0 +1,34 @@
+//
+//  VividShinaTests.m
+//  VividShinaTests
+//
+//  Created by icreative-mini on 13-11-27.
+//  Copyright (c) 2013年 icreative-mini. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VividShinaTests : XCTestCase
+
+@end
+
+@implementation VividShinaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
